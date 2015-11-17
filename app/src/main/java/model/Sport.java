5870973f04d43657c9bb8876ab6013f2bd2ee7b8
Guillem Barbosa <guillembarbosa@gmail.com>
2015-11-17 19:24:37 +0100

    Getters, setters i constructor

--- app/src/main/java/model/Sport.java
+++ app/src/main/java/model/Sport.java
@@ -12,4 +12,48 @@ public class Sport {
     private int minPlayers;
     private int maxPlayers;
 
+    public Sport(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMinPlayers() {
+        return minPlayers;
+    }
+
+    public void setMinPlayers(int minPlayers) {
+        this.minPlayers = minPlayers;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public void setMaxPlayers(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
 }
