eef871d08e30d1d99909d0562f85cfff942b1dea
Lluís Echeverria <llecheverria@sosteca.es>
2015-11-18 19:36:42 +0100

    new event wizard

---
+++ app/src/main/java/General/Globals.java
@@ -0,0 +1,11 @@
+package General;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by Lluís on 18/11/2015.
+ */
+public class Globals {
+
+    public static SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm");
+}
--- app/src/main/java/com/ssn/eps/ssn/activities/NewEventWizardActivity.java
+++ app/src/main/java/com/ssn/eps/ssn/activities/NewEventWizardActivity.java
@@ -16,6 +16,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -26,7 +29,15 @@ import android.widget.TimePicker;
 
 import com.ssn.eps.ssn.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import General.Globals;
+import model.Sport;
 
 public class NewEventWizardActivity extends AppCompatActivity {
 
@@ -38,11 +49,12 @@ public class NewEventWizardActivity extends AppCompatActivity {
     private LayoutInflater layoutInflater;
 
     private int stepWizard = 0;
+    private List<String> sportsList = new ArrayList<String>();
 
     private Spinner sportsSpinner;
     private EditText numPlayersEditText;
     private EditText maxPricePlayerEditText;
-    private EditText dateHourEditText;
+    public EditText dateHourEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +78,18 @@ public class NewEventWizardActivity extends AppCompatActivity {
         fieldButton = (Button) findViewById(R.id.zone_button);
         summaryButton = (Button) findViewById(R.id.summary_button);
 
+
+        sportsList.add("Futbol");
+        sportsList.add("Tennis");
+        initializeStepViews(stepWizard);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     public void handleClick(View v){
@@ -86,30 +110,70 @@ public class NewEventWizardActivity extends AppCompatActivity {
                 flContainer.addView(layoutInflater.inflate(R.layout.content_new_event_wizard_step_3, vg));
                 stepWizard = 2;
                 break;
-            case (R.id.date_hour_edit_text):
-                showDatePickerDialog(null);
-                break;
             default:
                 break;
         }
     }
 
-    public void showTimePickerDialog(View v) {
-        DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "timePicker");
+    private void initializeStepViews(int step){
+        switch (step){
+            case 0:
+                sportsSpinner = (Spinner)findViewById(R.id.sports_spinner);
+                //http://stackoverflow.com/questions/867518/how-to-make-an-android-spinner-with-initial-text-select-one
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, sportsList);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                sportsSpinner.setAdapter(adapter);
+
+                numPlayersEditText = (EditText) findViewById(R.id.num_players_edit_text);
+
+                maxPricePlayerEditText =(EditText) findViewById(R.id.max_price_player_edit_text);
+
+                dateHourEditText = (EditText) findViewById(R.id.date_hour_edit_text);
+                dateHourEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View v, boolean hasFocus) {
+                        if(hasFocus){
+                            showDatePickerDialog(null);
+                        }
+                    }
+                });
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+
+        }
     }
+
+    public void setDateEditText(Calendar calendar){
+        dateHourEditText.setText(Globals.sdf.format(calendar.getTime()));
+        dateHourEditText.clearFocus();
+    }
+    public String getDateEditText(){
+        return dateHourEditText.getText().toString();
+    }
+
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
-    public static class TimePickerFragment extends DialogFragment
+    private static class TimePickerFragment extends DialogFragment
             implements TimePickerDialog.OnTimeSetListener {
+
+        private Calendar c;
+        public TimePickerFragment(Calendar calendar){
+            super();
+            this.c = calendar;
+        }
 
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the current time as the default values for the picker
-            final Calendar c = Calendar.getInstance();
             int hour = c.get(Calendar.HOUR_OF_DAY);
             int minute = c.get(Calendar.MINUTE);
 
@@ -119,16 +183,30 @@ public class NewEventWizardActivity extends AppCompatActivity {
         }
 
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            // Do something with the time chosen by the user
+            c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            c.set(Calendar.MINUTE,minute);
+            ((NewEventWizardActivity)getActivity()).setDateEditText(c);
         }
     }
-    public static class DatePickerFragment extends DialogFragment
+
+    private static class DatePickerFragment extends DialogFragment
             implements DatePickerDialog.OnDateSetListener {
+
+        private Calendar c;
 
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
+
+            String date = ((NewEventWizardActivity)getActivity()).getDateEditText();
+            c = Calendar.getInstance();
+            if(!date.isEmpty()){
+                try {
+                    c.setTime(Globals.sdf.parse(date));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
             int year = c.get(Calendar.YEAR);
             int month = c.get(Calendar.MONTH);
             int day = c.get(Calendar.DAY_OF_MONTH);
@@ -138,7 +216,13 @@ public class NewEventWizardActivity extends AppCompatActivity {
         }
 
         public void onDateSet(DatePicker view, int year, int month, int day) {
-            //showTimePickerDialog(null);
+            if(view.isShown()){
+                c.set(Calendar.YEAR,year);
+                c.set(Calendar.MONTH,month);
+                c.set(Calendar.DAY_OF_MONTH,day);
+                DialogFragment newFragment = new TimePickerFragment(c);
+                newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+            }
         }
     }
 }
